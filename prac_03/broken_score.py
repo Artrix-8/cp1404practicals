6e604553de1e7873453549c839939c9eb04ee4c9
Artrix-8 <69241278+Artrix-8@users.noreply.github.com>
2021-08-20 21:16:59 +1000

    update to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,16 +2,36 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-# Fixed
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-else:
-    if score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Pass")
+def main():
+    score = get_user_score()
+    print_score(score)
+    score = get_rand_score()
+    print_score(score)
+
+
+def print_score(score):
+    if score < 0 or score > 100:
+        print("Invalid score")
     else:
-        print("Bad")
+        if score >= 90:
+            print("Excellent")
+        elif score >= 50:
+            print("Pass")
+        else:
+            print("Bad")
+
+
+def get_user_score():
+    score = float(input("Enter score: "))
+    return score
+
+
+def get_rand_score():
+    score = random.randint(0, 100)
+    return score
+
+
+main()
