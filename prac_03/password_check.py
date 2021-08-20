7fabac41510bfdddc852297ed72ad07a37e2ea6f
Artrix-8 <69241278+Artrix-8@users.noreply.github.com>
2021-08-20 20:52:41 +1000

    refactor program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -2,11 +2,20 @@ MIN_PASS = 4
 
 
 def main():
+    password = get_password()
+    print_password_secret(password)
+
+
+def print_password_secret(password):
+    print('*' * len(password))
+
+
+def get_password():
     password = input(f"Create Password (must be {MIN_PASS} characters long): ")
     while len(password) < MIN_PASS:
         print("Password too short!!!")
         password = input("Enter password of at least {} characters: ".format(MIN_PASS))
-    print('*' * len(password))
+    return password
 
 
 main()
