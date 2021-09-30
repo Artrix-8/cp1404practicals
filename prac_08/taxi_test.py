9a35c8001f517428dd6f73184c8f7f687b46920e
Artrix-8 <69241278+Artrix-8@users.noreply.github.com>
2021-10-01 00:11:06 +1000

    change price_per_km to a class variable

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -4,7 +4,7 @@ Code for testing 'taxi' class
 
 from prac_08.taxi import Taxi
 
-new_taxi = Taxi("Prius 1", 100, 1.23)  # Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km
+new_taxi = Taxi("Prius 1", 100)  # Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km
 new_taxi.drive(40)  # Drive the taxi 40 km
 print(new_taxi)  # Print the taxi's details and the current fare
 print(new_taxi.get_fare())
