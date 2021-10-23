b4f2f9cc88861faa535cdc319c910939360d4ad5
Artrix-8 <69241278+Artrix-8@users.noreply.github.com>
2021-10-23 22:02:59 +1000

    Add extra logic to file correcting NOTE: It'syourblood.txt current IMPOSSIBLE to correct

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -7,7 +7,28 @@ import os
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    filename = filename.replace(" ", "_").replace(".TXT", ".txt")
+
+    new_name = ""
+    for index, character in enumerate(filename):
+        try:
+            if character.islower() and filename[index-1] == '_':
+                new_name += character.upper()
+            elif character.islower() and filename[index-1] == '(':
+                new_name += character.upper()
+            elif not new_name:
+                new_name += character.upper()
+            else:
+                new_name += character
+            if character.islower() and filename[index+1].isupper():
+                new_name += '_'
+            if character.islower() and filename[index+1] == '(':
+                new_name += '_'
+            if character.isupper() and filename[index + 1].isupper():
+                new_name += '_'
+        except IndexError:
+            pass
+    print(new_name)
     return new_name
 
 
@@ -15,10 +36,12 @@ def demo_walk():
     """Process all subdirectories using os.walk()."""
     os.chdir('Lyrics')
     for directory_name, subdirectories, filenames in os.walk('.'):
+        """
         print("Directory:", directory_name)
         print("\tcontains subdirectories:", subdirectories)
         print("\tand files:", filenames)
         print("(Current working directory is: {})".format(os.getcwd()))
+        """
 
         for filename in filenames:
             full_name = os.path.join(directory_name, filename)
