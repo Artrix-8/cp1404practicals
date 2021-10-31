860dd0263182fbbf421e7622a8e620cd527af56d
Artrix-8 <69241278+Artrix-8@users.noreply.github.com>
2021-10-31 11:14:38 +1000

    Add printing information with exception handling

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,6 +1,14 @@
 import wikipedia
 
-page_title = input("Page title: ")
-while page_title:
-    print(page_title)
-    page_title = input("Page title: ")
+title = input("Page title: ")
+while title:
+    try:
+        page = wikipedia.page(title, auto_suggest=False)
+        print(page.title)
+        print(page.summary)
+        print(page.url)
+    except wikipedia.exceptions.DisambiguationError as e:
+        print(e.options)
+    except wikipedia.exceptions.PageError:
+        print(f"\"{title}\" does not match any pages. Try another query!")
+    title = input("Page title: ")
